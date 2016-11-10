386f1efe7f32a20d873e5a4ca9873a0e50761212
SaltyHash <austin.bowen.314@gmail.com>
2016-11-09 19:52:58 -0500

    Create AppSettings.m

---
+++ AppSettings.m
@@ -0,0 +1,59 @@
+//
+//  AppSettings.m
+//
+//  Created by Austin Bowen on 6/6/15.
+//
+
+#import "AppSettings.h"
+
+@implementation AppSettings
+
++ (instancetype)sharedInstance
+{
+    // Class-agnostic code
+    static dispatch_once_t onceToken;
+    static id              singleton = nil;
+    dispatch_once(&onceToken, ^{
+        singleton = [[[self class] alloc] init];
+    });
+    return singleton;
+}
+
+- (instancetype)init
+{
+    if (!(self = [super init])) return nil;
+    
+    self.autoSync = YES;
+    
+    return self;
+}
+
+#pragma mark - Methods
+
+- (void)resetToDefaults
+{
+    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:[[NSBundle mainBundle] bundleIdentifier]];
+}
+
+- (BOOL)synchronize
+{
+    return [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+#pragma mark - Properties
+
+- (NSString *)someSetting
+{
+    return [[NSUserDefaults standardUserDefaults] stringForKey:@"someSetting"];
+}
+
+- (void)setSomeSetting:(NSString *)someSetting
+{
+    [[NSUserDefaults standardUserDefaults] setObject:someSetting
+                                              forKey:@"someSetting"];
+    if (self.autoSync) [self synchronize];
+}
+
+// Repeat for additional settings...
+
+@end
